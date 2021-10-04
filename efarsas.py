260c6ee8c0e1733b4773c2603677d7007a0ff9a7
Arthur Uezu <uezuarthur@gmail.com>
2021-10-04 19:37:13 -0300

    código mais limpo, organizado e re-testado!

--- efarsas.py
+++ efarsas.py
@@ -8,78 +8,65 @@ def tratamentoData(data):
     date[1] = mes_dicionario[date[1]]
     return date
 
+def filtragemCategoria(categoria):
+    try:
+        if categoria.text=="Conspirações" or categoria.text=="Falso":
+            categoria = "Falso"
+        else:
+            categoria = "Verdadeiro"
+    except(AttributeError):
+        categoria = "Erro"
+    return categoria
+
+mes_dicionario = {'janeiro':1,'fevereiro':2,'março':3,'abril':4,'maio':5,'junho':6,'julho':7,'agosto':8,'setembro':9,'outubro':10,'novembro':11,'dezembro':12}
 palavras_chave = ("corona", "coronavirus", "sars-cov-2", "covid") 
 
 url_base = "https://www.e-farsas.com/page/"
 url_final = "?s="
-url_site = "https://www.e-farsas.com/page/1?s="
 
 #lista para armazenar os dados recolhidos do site
 dados = []
-mes_dicionario = {'janeiro':1,'fevereiro':2,'março':3,'abril':4,'maio':5,'junho':6,'julho':7,'agosto':8,'setembro':9,'outubro':10,'novembro':11,'dezembro':12}
-
 
 #loop principal do webscraper
 for palavra in palavras_chave:
-    data = [4,10,2021]
-    j=1 #pagina inicial
-    flag = True
+    pagina_atual=1
+    paginas_total=2
 
     print('Palavra chave atual: ' + palavra)
 
-    while flag:
-        response_menu = requests.get(url_base+str(j)+url_final+palavra) #site + palavra chave
-        print("Pagina:",j,"\n")
+    while pagina_atual != int(paginas_total) + 1:
+        response_menu = requests.get(url_base+str(pagina_atual)+url_final+palavra) #site + palavra chave
         site_menu = BeautifulSoup(response_menu.text,'html.parser')
         container = site_menu.find('div', attrs={'class': 'td_block_inner tdb-block-inner td-fix-index'})
-        #print(container.text)
-
-        #LEMBRA DE MUDAR PRA CONTAINER
         noticias = container.find_all('div',attrs={'class':'td-module-meta-info'}) #recolhe cada box de noticia
 
+        print("Pagina:",pagina_atual)
         #pega o numero de paginas total
-        pag_total = site_menu.find('span', attrs={'class': 'pages'})
-        pag_total_text = pag_total.text
-        pag_total_text = pag_total_text.split(" de ") #lista [pagina atual, pagina final]    
+        paginas_total = site_menu.find('span', attrs={'class': 'pages'})
+        paginas_total = paginas_total.text.split(" de ")[1] #lista [pagina atual, pagina final
         
         #seleção das informações relevantes
-        for noticia in noticias:          
-            
+        for noticia in noticias:
             data = noticia.find('time')
             data = tratamentoData(data.text)
-            
-            if(int(data[2]) <= 2019):
-                flag = False
+            if int(data[2]) <= 2019:
+                pagina_atual = int(paginas_total)
                 break
 
-            try:
-                titulo = noticia.find('a',attrs={'rel':'bookmark'})
-                print("Post: " + titulo.text)
-                
-            except(IndexError):
-                data[2]=0
-                break
-            
+            titulo = noticia.find('a',attrs={'rel':'bookmark'})
+            print("Post: " + titulo.text)
+
             categoria = noticia.find('a',attrs={'class':'td-post-category'})
-            try:
-                if(categoria.text=="Conspirações" or categoria.text=="Falso"):
-                    categoria = "Falso"
-                else:
-                    categoria = "Verdadeiro"
-            except(AttributeError):
-                categoria = "Erro"
-                continue
-            link = titulo['href']
-            response_post = requests.get(link)
-            site_post = BeautifulSoup(response_post.text,'html.parser')
-            noticia_texto = site_post.find("div", attrs={"class": "td_block_wrap tdb_single_content tdi_98 td-pb-border-top td_block_template_1 td-post-content tagdiv-type"})
-            dados.append([link,titulo.text,categoria,data[0]+'/'+str(data[1])+'/'+data[2],noticia_texto.text]) #armazenamento dos dados na lista
-        j = j + 1
+            categoria = filtragemCategoria(categoria)
 
-        #acaba o loop da palavra atual (quando acabar as paginas)
-        if j == int(pag_total_text[1]) + 1:
-            flag = False
-            break
+            link_noticia = titulo['href']
+            response_noticia = requests.get(link_noticia)
+            site_noticia = BeautifulSoup(response_noticia.text,'html.parser')
+            noticia_texto = site_noticia.find("div", attrs={"class": "td_block_wrap tdb_single_content tdi_98 td-pb-border-top td_block_template_1 td-post-content tagdiv-type"})
+            
+            dados.append([link_noticia,titulo.text,categoria,data[0]+'/'+str(data[1])+'/'+data[2],noticia_texto.text]) #armazenamento dos dados na lista
+        pagina_atual = pagina_atual + 1
+        print('\n')
 
 #remoção de dados duplicados
 dados = pd.DataFrame(dados,columns=['link','titulo','categoria','data','texto']).drop_duplicates()
